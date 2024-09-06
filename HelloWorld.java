2fe1b7b0bb2916253f9cda0902fd52ba79c5b778
AJ Mrotek <mrotekaw@beloit.edu>
2024-09-06 13:42:10 -0500

    Added hello world print statement to java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ package myRepo;
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Brendan Drager");
+        //Changes Made by AJ Mrotek: 
+        System.out.println("Hello, World");
     }
 }
